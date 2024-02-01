7f1cf6c989b788b734c9b57d312bc00296078545
Samuel-Tobi <oluwatobi9990@gmail.com>
2024-02-01 12:17:32 -0600

    Created NewsLetter Component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Hero from "../components/hero/Hero";
-import Popular from "../components/popular/Popular";
-import Offers from "../components/offers/Offers";
 import NewCollections from "../components/newCollections/NewCollections";
+import NewsLetter from "../components/newsLetter/NewsLetter";
+import Offers from "../components/offers/Offers";
+import Popular from "../components/popular/Popular";
 
 function Shop() {
   return (
@@ -11,6 +12,7 @@ function Shop() {
       <Popular />
       <Offers />
       <NewCollections />
+      <NewsLetter />
     </div>
   );
 }
