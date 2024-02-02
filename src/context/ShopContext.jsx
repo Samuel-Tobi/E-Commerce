13a238e50e2abea88b94730b5005d3392d0218b0
Samuel-Tobi <oluwatobi9990@gmail.com>
2024-02-02 00:03:26 -0600

    Add ShopContextProvider to manage product data

---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,15 @@
+import React, { createContext } from "react";
+import allProducts from "../components/assets/all_product";
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+  const contextValue = { allProducts };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
