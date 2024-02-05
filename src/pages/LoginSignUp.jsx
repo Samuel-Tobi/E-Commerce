1783bf222f6e42fd6d45b1cbe25685fafc3b745e
Samuel-Tobi <oluwatobi9990@gmail.com>
2024-02-05 02:23:06 -0600

    Signup component designed

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./CSS/loginSignup.css";
 
 function LoginSignUp() {
-  return <div></div>;
+  return (
+    <div className="loginSignup">
+      <div className="loginSignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginSignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address " />
+          <input type="password" placeholder="Password " />
+        </div>
+        <button>Continue</button>
+        <p className="loginSignup-login">
+          Already have an account? <span>Login</span>
+        </p>
+        <div className="loginSignup-agree">
+          <input type="checkbox" name="continue" id="continue" />
+          <label htmlFor="continue">
+            By continuing, i agree to the terms of use &amp; privacy policy.
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginSignUp;
